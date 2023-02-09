02140ec258fa7356c467c9329c834230bc8f5a03
Sraznoff <sraznoff@headwatersgeographic.com>
2023-02-08 20:40:23 -0700

    Got the models working and finished vid 2

--- djangodb/website/admin.py
+++ djangodb/website/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Member, Orgazniation
 
-# Register your models here.
+admin.site.register(Member)
+admin.site.register(Orgazniation)
--- djangodb/website/models.py
+++ djangodb/website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+     fname = models.CharField(max_length=50, null= True)
+     lname = models.CharField(max_length=100, null=True)
+     email = models.EmailField(max_length=100, null=True)
+     passwd = models.CharField(max_length=50, null=True)
+
+     def __str__(self):
+          return self.fname + ' ' + self.lname
+
+class Orgazniation(models.Model):
+    orgName = models.CharField(max_length=50, null=True)
+    def __str__(self):
+         return self.orgName
+    
