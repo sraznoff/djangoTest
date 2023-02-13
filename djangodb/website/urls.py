783e9fa788cf7a1b64496bc223c920e6a762c150
Sraznoff <sraznoff@headwatersgeographic.com>
2023-02-12 21:13:01 -0700

    Added a form

--- djangodb/website/urls.py
+++ djangodb/website/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('join', views.join, name="join")
 ]
--- djangodb/website/views.py
+++ djangodb/website/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-
+from .models import Member
 def home(request):
-    return render(request, 'home.html', {})
+    all_members = Member.objects.all
+    return render(request, 'home.html', {'all': all_members})
+
+def join(request):
+    return render(request, 'join.html', {})
